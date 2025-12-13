02d8e724c726b8b6448e960a7de94a3e4384f133
Nur171 <nuraksoy2023@gmail.com>
2025-12-13 09:28:20 +0300

    Oop Example

---
+++ src/Cat.java
@@ -0,0 +1,31 @@
+public class Cat {
+    int x;
+    double direction;
+    String name;
+
+    public Cat(){
+        this(0,180,"Sofya");
+    }
+    public Cat(int x, double direction, String name){
+        this.x = x;
+        this.direction = direction;
+        this.name = name;
+    }
+
+    public void move(){
+        x += 20;
+    }
+
+    public void move2(int a){
+        x += a;
+    }
+
+    public void turn(double angle){
+        direction += angle;
+    }
+
+    public void status(){
+        System.out.println("Name: " + name + "\nx: " + x + "\nDirection: " + direction);
+        System.out.println("*****************************");
+    }
+}
